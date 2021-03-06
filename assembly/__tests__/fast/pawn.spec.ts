cc8081593994efe0e7f6fa8fcc91cf1e40a0b56d
Alex Victoor <alexvictoor@gmail.com>
2021-03-06 23:28:58 +0100

    pawn move generation

---
+++ assembly/__tests__/fast/pawn.spec.ts
@@ -0,0 +1,105 @@
+import {
+  BISHOP,
+  BitBoard,
+  BLACK,
+  encodeCapture,
+  encodeMove,
+  KNIGHT,
+  PAWN,
+  QUEEN,
+  ROOK,
+  WHITE,
+} from "../../fast/bitboard";
+import { addPawnPseudoLegalMoves } from "../../fast/pawn";
+
+describe(`Pawn move generation`, () => {
+  it("should get pawn move one square forward when nothing is in the way", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(PAWN, BLACK, 16);
+    board.putPiece(PAWN, BLACK, 24);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, BLACK);
+    // then
+    expect(moves).toHaveLength(1);
+    expect(moves).toContain(encodeMove(PAWN + BLACK, 16, PAWN + BLACK, 8));
+  });
+
+  it("should get no pawn move when there is a piece in the way", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(PAWN, WHITE, 16);
+    board.putPiece(PAWN, BLACK, 24);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, BLACK);
+    // then
+    expect(moves).toHaveLength(0);
+  });
+
+  it("should capture on the side", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(PAWN, WHITE, 8);
+    board.putPiece(PAWN, WHITE, 9);
+    board.putPiece(PAWN, WHITE, 10);
+    board.putPiece(PAWN, BLACK, 17);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, BLACK);
+    // then
+    expect(moves).toHaveLength(2);
+  });
+
+  it("should get pawn promotions when pawns reach last row", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(PAWN, WHITE, 16);
+    board.putPiece(PAWN, BLACK, 8);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, BLACK);
+    // then
+    expect(moves).toHaveLength(4);
+    expect(moves).toContain(encodeMove(PAWN + BLACK, 8, QUEEN + BLACK, 0));
+    expect(moves).toContain(encodeMove(PAWN + BLACK, 8, ROOK + BLACK, 0));
+    expect(moves).toContain(encodeMove(PAWN + BLACK, 8, BISHOP + BLACK, 0));
+    expect(moves).toContain(encodeMove(PAWN + BLACK, 8, KNIGHT + BLACK, 0));
+  });
+  it("should get pawn promotions when pawns captures on last row", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(ROOK, WHITE, 0);
+    board.putPiece(ROOK, WHITE, 1);
+    board.putPiece(PAWN, BLACK, 8);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, BLACK);
+    // then
+    expect(moves).toHaveLength(4);
+    expect(moves).toContain(
+      encodeCapture(PAWN + BLACK, 8, QUEEN + BLACK, 1, ROOK + WHITE, 1)
+    );
+    expect(moves).toContain(
+      encodeCapture(PAWN + BLACK, 8, ROOK + BLACK, 1, ROOK + WHITE, 1)
+    );
+    expect(moves).toContain(
+      encodeCapture(PAWN + BLACK, 8, BISHOP + BLACK, 1, ROOK + WHITE, 1)
+    );
+    expect(moves).toContain(
+      encodeCapture(PAWN + BLACK, 8, KNIGHT + BLACK, 1, ROOK + WHITE, 1)
+    );
+  });
+
+  it("should get pawn move one or two squares forward from initial position", () => {
+    // given
+    const board = new BitBoard();
+    board.putPiece(PAWN, WHITE, 8);
+    // when
+    const moves: u64[] = [];
+    addPawnPseudoLegalMoves(moves, board, WHITE);
+    // then
+    expect(moves).toHaveLength(2);
+  });
+});
--- assembly/fast/pawn.ts
+++ assembly/fast/pawn.ts
@@ -1,7 +1,192 @@
-import { leftBorderMask, rightBorderMask } from "./bitboard";
+import {
+  BLACK,
+  WHITE,
+  leftBorderMask,
+  rightBorderMask,
+  BISHOP,
+  BitBoard,
+  encodeCapture,
+  encodeMove,
+  getPositionsFromMask,
+  KNIGHT,
+  opponent,
+  PAWN,
+  QUEEN,
+  ROOK,
+} from "./bitboard";
 
+const whiteInitialRowMask: u64 =
+  (1 << 8) |
+  (1 << 9) |
+  (1 << 10) |
+  (1 << 11) |
+  (1 << 12) |
+  (1 << 13) |
+  (1 << 14) |
+  (1 << 15);
+
+const blackInitialRowMask: u64 =
+  (1 << 48) |
+  (1 << 49) |
+  (1 << 50) |
+  (1 << 51) |
+  (1 << 52) |
+  (1 << 53) |
+  (1 << 54) |
+  (1 << 55);
+
+export function pawnInitialRowMask(player: i8): u64 {
+  return player === WHITE ? whiteInitialRowMask : blackInitialRowMask;
+}
+
+export function pawnAttacksOnLeft(player: i8, pawnMask: u64): u64 {
+  return player === BLACK
+    ? (pawnMask >> 7) & leftBorderMask
+    : (pawnMask << 7) & rightBorderMask;
+}
+
+export function pawnAttacksOnRight(player: i8, pawnMask: u64): u64 {
+  return player === BLACK
+    ? (pawnMask >> 9) & rightBorderMask
+    : (pawnMask << 9) & leftBorderMask;
+}
 export function pawnAttacks(player: i8, pawnMask: u64): u64 {
-  return player
+  return player === BLACK
     ? ((pawnMask >> 7) & leftBorderMask) | ((pawnMask >> 9) & rightBorderMask)
     : ((pawnMask << 7) & rightBorderMask) | ((pawnMask << 9) & leftBorderMask);
+}
+
+const isInLastRow = (player: i8, position: i8): boolean =>
+  player === WHITE ? position > 55 : position < 8;
+
+const oneSquareMoveMask = (player: i8, pawnMask: u64, allMask: u64): u64 =>
+  player === WHITE ? (pawnMask << 8) & ~allMask : (pawnMask >> 8) & ~allMask;
+
+const promotionTargets: i8[] = [QUEEN, ROOK, BISHOP, KNIGHT];
+
+export function addPawnPseudoLegalMoves(
+  moves: u64[],
+  board: BitBoard,
+  player: i8
+): void {
+  const pawnMask = board.getPawnMask(player);
+
+  const positions = getPositionsFromMask(
+    oneSquareMoveMask(player, pawnMask, board.getAllPiecesMask())
+  );
+  const direction: i8 = player === WHITE ? 1 : -1;
+  for (let i = 0; i < positions.length; i++) {
+    const targetPosition = positions[i];
+    const currentPosition = targetPosition - direction * 8;
+
+    if (isInLastRow(player, positions[i])) {
+      for (let j = 0; j < promotionTargets.length; j++) {
+        moves.push(
+          encodeMove(
+            PAWN + player,
+            currentPosition,
+            promotionTargets[j] + player,
+            targetPosition
+          )
+        );
+      }
+    } else {
+      moves.push(
+        encodeMove(
+          PAWN + player,
+          currentPosition,
+          PAWN + player,
+          targetPosition
+        )
+      );
+    }
+  }
+
+  const captureMaskOnLeft =
+    pawnAttacksOnLeft(player, pawnMask) &
+    board.getPlayerPiecesMask(opponent(player));
+  const pawnCapturingMaskOnLeft =
+    pawnAttacks(opponent(player), captureMaskOnLeft) &
+    board.getPlayerPiecesMask(player);
+  const pawnCapturinLeftPositions = getPositionsFromMask(
+    pawnCapturingMaskOnLeft
+  );
+  for (let i = 0; i < pawnCapturinLeftPositions.length; i++) {
+    const capturePosition = pawnCapturinLeftPositions[i] + direction * 7;
+    if (isInLastRow(player, capturePosition)) {
+      for (let j = 0; j < promotionTargets.length; j++) {
+        moves.push(
+          encodeCapture(
+            PAWN + player,
+            pawnCapturinLeftPositions[i],
+            promotionTargets[j] + player,
+            capturePosition,
+            board.getPieceAt(capturePosition)
+          )
+        );
+      }
+    } else {
+      moves.push(
+        encodeCapture(
+          PAWN + player,
+          pawnCapturinLeftPositions[i],
+          PAWN + player,
+          capturePosition,
+          board.getPieceAt(capturePosition)
+        )
+      );
+    }
+  }
+  const captureMaskOnRight =
+    pawnAttacksOnRight(player, pawnMask) &
+    board.getPlayerPiecesMask(opponent(player));
+  const pawnCapturingMaskOnRight =
+    pawnAttacks(opponent(player), captureMaskOnRight) &
+    board.getPlayerPiecesMask(player);
+  const pawnCapturinRightPositions = getPositionsFromMask(
+    pawnCapturingMaskOnRight
+  );
+  for (let i = 0; i < pawnCapturinRightPositions.length; i++) {
+    const capturePosition = pawnCapturinRightPositions[i] + direction * 9;
+    if (isInLastRow(player, capturePosition)) {
+      for (let j = 0; j < promotionTargets.length; j++) {
+        moves.push(
+          encodeCapture(
+            PAWN + player,
+            pawnCapturinRightPositions[i],
+            promotionTargets[j] + player,
+            capturePosition,
+            board.getPieceAt(capturePosition)
+          )
+        );
+      }
+    } else {
+      moves.push(
+        encodeCapture(
+          PAWN + player,
+          pawnCapturinRightPositions[i],
+          PAWN + player,
+          capturePosition,
+          board.getPieceAt(capturePosition)
+        )
+      );
+    }
+  }
+
+  const pawnsOnInitialRowMask = pawnMask & pawnInitialRowMask(player);
+  const pawnsMovingTwiceMask = oneSquareMoveMask(
+    player,
+    oneSquareMoveMask(player, pawnsOnInitialRowMask, board.getAllPiecesMask()),
+    board.getAllPiecesMask()
+  );
+  const pawnsMovingTwicePositions: i8[] = getPositionsFromMask(
+    pawnsMovingTwiceMask
+  );
+  for (let i = 0; i < pawnsMovingTwicePositions.length; i++) {
+    const targetPosition = positions[i];
+    const currentPosition = targetPosition - direction * 16;
+    moves.push(
+      encodeMove(PAWN + player, currentPosition, PAWN + player, targetPosition)
+    );
+  }
 }
