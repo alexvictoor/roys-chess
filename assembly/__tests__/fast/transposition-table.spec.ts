0823a314ff7b9567b4653407852588ed5c2ecbd3
Alex Victoor <alexvictoor@gmail.com>
2021-04-28 22:36:35 +0200

    transposition table

---
+++ assembly/__tests__/fast/transposition-table.spec.ts
@@ -0,0 +1,30 @@
+import { WHITE } from "../../fast/bitboard";
+import { pseudoLegalMoves } from "../../fast/engine";
+import { parseFEN } from "../../fast/fen-parser";
+import {
+  ALPHA_SCORE,
+  decodeDepthFromEntry,
+  decodeMoveFromEntry,
+  decodeScoreFromEntry,
+  decodeScoreTypeFromEntry,
+  TranspositionTable,
+} from "../../fast/transposition-table";
+
+describe("Transposition table", () => {
+  it("should store data", () => {
+    // give
+    const table = new TranspositionTable(3);
+    const board = parseFEN(
+      "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+    );
+    const moves = pseudoLegalMoves(board, WHITE);
+    // when
+    table.record(board, moves[0], 42, ALPHA_SCORE, 4);
+    // then
+    const entry = table.getEntry(board);
+    expect(decodeMoveFromEntry(entry)).toBe(moves[0]);
+    expect(decodeScoreFromEntry(entry)).toBe(42);
+    expect(decodeScoreTypeFromEntry(entry)).toBe(ALPHA_SCORE);
+    expect(decodeDepthFromEntry(entry)).toBe(4);
+  });
+});
---
+++ assembly/fast/transposition-table.ts
@@ -0,0 +1,61 @@
+import { BitBoard } from "./bitboard";
+
+export const EXACT_SCORE: i8 = 0;
+export const ALPHA_SCORE: i8 = 1;
+export const BETA_SCORE: i8 = 2;
+
+export class TranspositionTable {
+  verificationEntries: StaticArray<u64>;
+  moveEntries: StaticArray<u64>;
+  size: i32;
+
+  constructor(sizeMagnitude: i32 = 26) {
+    this.size = 1 << sizeMagnitude;
+    this.verificationEntries = new StaticArray<u64>(this.size);
+    this.moveEntries = new StaticArray<u64>(this.size);
+  }
+
+  record(
+    board: BitBoard,
+    move: u32,
+    score: i16,
+    scoreType: i8,
+    depth: i8
+  ): void {
+    const hash = board.hashCode();
+    const verificationEntry: u64 = (<u64>depth) | ((hash >> 5) << 5);
+    const index: i32 = <i32>(hash % this.size);
+    this.verificationEntries[index] = verificationEntry;
+    this.moveEntries[index] =
+      (<u64>scoreType) |
+      ((<u64>move) << 2) |
+      ((<u64>score) << 32) |
+      ((<u64>depth) << 48);
+  }
+
+  getEntry(board: BitBoard): u64 {
+    const hash = board.hashCode();
+    const index = <i32>(hash % this.size);
+    const verificationEntry = this.verificationEntries[index];
+    const hashVerified = verificationEntry >> 5 == hash >> 5;
+    if (hashVerified) {
+      return this.moveEntries[index];
+    }
+    return 0;
+  }
+
+  reset(): void {}
+}
+
+export function decodeMoveFromEntry(entry: u64): u32 {
+  return <u32>((entry >> 2) & 0b00111111111111111111111111111111);
+}
+export function decodeScoreFromEntry(entry: u64): i16 {
+  return <i16>((entry >> 32) & 0xffff);
+}
+export function decodeScoreTypeFromEntry(entry: u64): i8 {
+  return <i8>(entry & 0b00000011);
+}
+export function decodeDepthFromEntry(entry: u64): i8 {
+  return <i8>(entry >> 48);
+}
